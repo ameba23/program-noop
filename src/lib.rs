f926458bd4d542dfb1f7d2ac5f9451f46026f70b
peg <ameba23@systemli.org>
2024-05-15 09:12:11 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+//! No-op program
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+extern crate alloc;
+
+use alloc::{string::ToString, vec::Vec};
+
+use entropy_programs_core::{bindgen::Error, bindgen::*, export_program, prelude::*};
+use serde::{Deserialize, Serialize};
+
+#[cfg(test)]
+mod tests;
+
+// TODO confirm this isn't an issue for audit
+register_custom_getrandom!(always_fail);
+
+/// JSON-deserializable struct that will be used to derive the program-JSON interface.
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct UserConfig {}
+
+/// JSON representation of the auxiliary data
+#[cfg_attr(feature = "std", derive(schemars::JsonSchema))]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct AuxData {}
+
+pub struct ProgramNoop;
+
+impl Program for ProgramNoop {
+    fn evaluate(
+        signature_request: SignatureRequest,
+        _config: Option<Vec<u8>>,
+        _oracle_data: Option<Vec<u8>>,
+    ) -> Result<(), Error> {
+        if signature_request.message.is_empty() {
+            return Err(Error::Evaluation(
+                "Message must have a length greater than zero".to_string(),
+            ));
+        }
+        Ok(())
+    }
+
+    /// Since we don't use a custom hash function, we can just return `None` here.
+    fn custom_hash(_data: Vec<u8>) -> Option<Vec<u8>> {
+        None
+    }
+}
+
+export_program!(ProgramNoop);
